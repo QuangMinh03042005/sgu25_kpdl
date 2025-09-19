9a64f05ff319e81588426b04d1108c04373beee0
QuangMinh03042005 <quangminh736.2020@gmail.com>
2025-09-19 18:59:41 +0700

    Lab01b have done!

---
+++ Lab01b/ex3_circle.py
@@ -0,0 +1,13 @@
+r = float(input("nhap vao ban kinh r la so thuc(m): "))
+
+
+PI = 3.14
+
+cv = 2 * PI * r
+dt = PI * r * r
+
+print(f"ban kinh r = {r} m")
+print("chu vi:")
+print(f"2 * PI * r = 2 * {PI} * {r} = {cv} m")
+print("dien tich:")
+print(f"PI * r * r = 2 * {PI} * {r * r} = {dt} m")
